e21d738d40ab569eb9ad2f66af48e016158b7a4d
imochasingh <75923058+imochasingh@users.noreply.github.com>
2023-10-03 12:28:25 +0530

    home page

--- local_superstore/Home.py
+++ local_superstore/Home.py
@@ -12,18 +12,18 @@ st.set_page_config(page_title="Raj's Superstore", page_icon="🌀", layout="cent
 for key in st.session_state:
     st.session_state[key] = st.session_state[key]
 
-st.title("Welcome to the Raj's Superstore analysis app!")
-st.sidebar.info("Choose a page!")
-st.markdown(
-    """
-This is a simple app to show the features of streamlit.
-It is using snowflake as the datasource 
+# st.title("Welcome to the Raj's Superstore analysis app!")
+# st.sidebar.info("Choose a page!")
+# st.markdown(
+#     """
+# This is a simple app to show the features of streamlit.
+# It is using snowflake as the datasource 
 
-### Get started!
+# ### Get started!
 
-👈 Select a page in the sidebar!
-    """
-)
+# 👈 Select a page in the sidebar!
+#     """
+# )
 
 
 def load_lottiefile(filepath: str):
@@ -36,16 +36,19 @@ with st.container():
     st.write("---")
     r1_c1, r2_c2 = st.columns(2)
     with r1_c1:
-        st.header(" What I do ")
+        st.header("Welcome to the Raj's Superstore analysis app!")
         st.write("##")
-        st.write("""
-                On my youtube chennel I am creating tutorials for:
-                 - Snowflake
-                 - Domo
-                 - dbt
-                 """
-                )
+      st.markdown(
+    """
+This is a simple app to show the features of streamlit.
+It is using snowflake as the datasource 
 
+### Get started!
+
+👈 Select a page in the sidebar!
+    """
+)
     with r2_c2:
         load_lottiefile("lottiefile.json")  # replace link to local lottie file
 
+st.write("---")
