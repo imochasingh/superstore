edcca2cbe6c449b60d0eaf59c3c13844bb9d107a
imochasingh <75923058+imochasingh@users.noreply.github.com>
2023-10-03 12:24:29 +0530

    home page

--- local_superstore/Home.py
+++ local_superstore/Home.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from streamlit_lottie import st_lottie
+import json
+
 
 st.set_page_config(page_title="Raj's Superstore", page_icon="🌀", layout="centered")
 
@@ -21,3 +24,28 @@ It is using snowflake as the datasource
 👈 Select a page in the sidebar!
     """
 )
+
+
+def load_lottiefile(filepath: str):
+    with open("lottiefile.json", "r") as f:
+        data = json.load(f)
+        st_lottie(data, height=400, width=400)
+
+
+with st.container():
+    st.write("---")
+    r1_c1, r2_c2 = st.columns(2)
+    with r1_c1:
+        st.header(" What I do ")
+        st.write("##")
+        st.write("""
+                On my youtube chennel I am creating tutorials for:
+                 - Snowflake
+                 - Domo
+                 - dbt
+                 """
+                )
+
+    with r2_c2:
+        load_lottiefile("lottiefile.json")  # replace link to local lottie file
+
